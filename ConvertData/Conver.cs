ddd42a98f740bd570d51fe7ae6259cb8f0ad5143
Leseim <41055469+Leseim@users.noreply.github.com>
2020-04-28 11:31:27 +0200

    GetFileExtension hinzugefügt

--- ConvertData/Conver.cs
+++ ConvertData/Conver.cs
@@ -19,6 +19,7 @@ namespace ConvertData
         bool json2xml(string InPath, string OutPath, string rootNodeXML);
         string Base64ToString(string Base64Text);
         string Base64PDFToZPL(string B64PDF, string password, int page, int dpi, int width, int height);
+        string GetFileExtension(string base64String);
 
         //Property
         string Status { get; set; }
@@ -129,6 +130,38 @@ namespace ConvertData
 
             return(_zplString);
         }
+
+        public string GetFileExtension(string base64String)
+        {
+            var data = base64String.Substring(0, 5);
+
+            switch (data.ToUpper())
+            {
+                case "IVBOR":
+                    return "png";
+                case "/9J/4":
+                    return "jpg";
+                case "AAAAF":
+                    return "mp4";
+                case "JVBER":
+                    return "pdf";
+                case "AAABA":
+                    return "ico";
+                case "UMFYI":
+                    return "rar";
+                case "E1XYD":
+                    return "rtf";
+                case "U1PKC":
+                    return "txt";
+                case "MQOWM":
+                case "77U/M":
+                    return "srt";
+                default:
+                    return string.Empty;
+            }
+        }
+
+
 
     }
 }
